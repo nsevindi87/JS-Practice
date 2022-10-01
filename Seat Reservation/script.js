a5391da204b4cf3542b0aa26758f323c3dca9b5a
Nizami Sevindi <nsevindi87@gmail.com>
2022-10-02 00:27:47 +0200

    Seat Reservation

--- Seat Reservation/script.js
+++ Seat Reservation/script.js
@@ -1,9 +1,29 @@
 const container = document.querySelector(".container");
+const count = document.getElementById("count");
+const amount = document.getElementById("amount");
+const select = document.getElementById("movie")
 
 
 container.addEventListener("click", function(e){
     if(e.target.classList.contains("seat") && !e.target.classList.contains("reserved")){    //Bos koltuklari sectim
-        e.target.classList.toggle("selected")
+        e.target.classList.toggle("selected");
+        calculateTotal()                                                //tiklama yaptikca hesaplama yapiyor
+    
     }
 
-})
+});
+
+
+
+select.addEventListener("change", function(e){ //option degistikce hesaplama yapiyor
+    calculateTotal()
+});
+
+
+
+function calculateTotal(){  //genel hesaplama fonksiyornu
+    let selectedSeatCount = container.querySelectorAll(".seat.selected").length;   //secilen koltuklarin sayisini aldik
+    let price = select.value;
+    count.innerText = selectedSeatCount;
+    amount.innerText = selectedSeatCount * price;
+}
