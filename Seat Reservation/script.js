18eedbe7bfb5654f3934863460d213ecc1573a9f
Nizami Sevindi <nsevindi87@gmail.com>
2022-10-03 22:29:32 +0200

    Seat Reservation

--- Seat Reservation/script.js
+++ Seat Reservation/script.js
@@ -1,8 +1,10 @@
 const container = document.querySelector(".container");
 const count = document.getElementById("count");
 const amount = document.getElementById("amount");
-const select = document.getElementById("movie")
+const select = document.getElementById("movie");
+const seats = document.querySelectorAll(".seat:not(.reserved)")
 
+getFromLocalStorage();
 
 container.addEventListener("click", function(e){
     if(e.target.classList.contains("seat") && !e.target.classList.contains("reserved")){    //Bos koltuklari sectim
@@ -22,8 +24,40 @@ select.addEventListener("change", function(e){ //option degistikce hesaplama yap
 
 
 function calculateTotal(){  //genel hesaplama fonksiyornu
+    let selectedSeats = container.querySelectorAll(".seat.selected");
+
+    let selectedSeatsArray = [];
+    let seatsArray = [];
+
+    selectedSeats.forEach(function(seat){
+        selectedSeatsArray.push(seat);
+    });
+
+    seats.forEach(function(seat){
+        seatsArray.push(seat)
+    });
+
+    let selectedSeatIndex = selectedSeatsArray.map(function(seat){ 
+        return seatsArray.indexOf(seat);
+    })
+
+    //secilmis olanlar array'a atildi. bütün koltuklar da array'a atildi. 
+    //secilmis arrayinde olanlari alip tüm koltuklar arasindaki index numarasi taratildi.
+
+    console.log(selectedSeatIndex)
+
     let selectedSeatCount = container.querySelectorAll(".seat.selected").length;   //secilen koltuklarin sayisini aldik
-    let price = select.value;
     count.innerText = selectedSeatCount;
-    amount.innerText = selectedSeatCount * price;
+    amount.innerText = selectedSeatCount * select.value;
+
+    savetoLocalStorage(selectedSeatIndex);
+}
+
+function getFromLocalStorage(){
+
+}
+
+function savetoLocalStorage(indexs){
+    localStorage.setItem("selectedSeats", JSON.stringify(indexs));
+    localStorage.setItem("selectedMovieIndex", select.selectedIndex);
 }
