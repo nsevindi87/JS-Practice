ff6c3dc83136c696acf3a526fe05fc90456bc510
Nizami Sevindi <nsevindi87@gmail.com>
2022-10-18 22:35:46 +0200

    completed

--- Weather-App-API/script.js
+++ Weather-App-API/script.js
@@ -2,8 +2,9 @@ const url = 'https://api.openweathermap.org/data/2.5/';
 const key = '869123b3c849a2a21c389a77bfac9e9e';
 
 const setQuery = (e)=>{
-    if(e.keyCode == '13')
+    if(e.keyCode == '13'){
         getResult(searchBar.value)
+    }
 }
 
 const getResult = (cityName)=>{
@@ -13,9 +14,11 @@ const getResult = (cityName)=>{
         return weather.json()
     })
     .then(displayResult)
+    
 }
 
 const displayResult = (result) => {
+    console.log(result)
     let city = document.querySelector('.city')
     city.innerText = `${result.name}, ${result.sys.country}`
 
@@ -23,13 +26,12 @@ const displayResult = (result) => {
     temp.innerText = `${Math.round(result.main.temp)}°C`
 
     let desc = document.querySelector('.desc')
-    desc.innerText = result.weather[0].description
+    desc.innerText = result.weather[0].main
 
     let minmax = document.querySelector('.minmax')
     minmax.innerText = `${Math.round(result.main.temp_min)}°C / ${Math.round(result.main.temp_max)} °C`
     
 }
-
 
 const searchBar = document.getElementById('searchBar')
 searchBar.addEventListener('keypress', setQuery)
