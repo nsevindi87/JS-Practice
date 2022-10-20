bb44dd003d869afec3d438d573f8b29e489f8d14
Nizami Sevindi <nsevindi87@gmail.com>
2022-10-21 01:03:05 +0200

    Operators are been added

--- Calculator/script.js
+++ Calculator/script.js
@@ -2,6 +2,9 @@ const display = document.querySelector('.calculator-input');
 const keys = document.querySelector('.calculator-keys');
 
 let displayValue = '0';
+let firstValue = null;
+let operator = null;
+let waitingForSecondValue =false;
 
 updateDisplay()
 
@@ -9,6 +12,7 @@ function updateDisplay(){
     display.value = displayValue
 }
 
+//-------------------------TUSLARIN EKRANA ALINMASI
 
 keys.addEventListener('click', function(e){
     const element = e.target;
@@ -16,9 +20,81 @@ keys.addEventListener('click', function(e){
     if(!element.matches('button')) return;
 
     if(element.classList.contains('operator')){
-        console.log('operator', element.value);
+        //console.log('operator', element.value);
+        handleOperator(element.value);
+        updateDisplay()
         return
     }
 
-    console.log(element)
-})
+    if(element.classList.contains('decimal')){
+        //console.log('decimal', element.value);
+        inputDecimal();
+        updateDisplay();
+        return;
+    }
+
+    if(element.classList.contains('clear')){
+        //console.log('clear', element.value);
+        clear()
+        updateDisplay()
+        return
+    }
+
+    //console.log('number',element.value)
+
+    inputNumber(element.value);
+    updateDisplay()
+});
+
+function handleOperator(nextOperator){
+    const value = parseFloat(displayValue);
+
+    if(operator && waitingForSecondValue){
+        operator = nextOperator;
+        return;
+    }
+
+    if(firstValue === null){
+        firstValue = value;
+    }else if (operator){
+        const result = calculate(firstValue, value, operator);
+
+        displayValue = `${parseFloat(result.toFixed(7))}`
+        firstValue = result;
+    }
+
+    waitingForSecondValue = true;
+    operator = nextOperator
+}
+
+function calculate(first, second, operator){
+    if(operator === '+'){
+        return first + second;
+    }else if(operator === '-'){
+        return first - second;
+    }else if(operator === '*'){
+        return first * second;
+    }else if(operator === '/'){
+        return first / second;
+    }
+    return second;
+}
+
+function inputNumber(num){
+    if(waitingForSecondValue){
+        displayValue = num;
+        waitingForSecondValue = false;
+    }else{
+        displayValue = displayValue === '0' ? num: displayValue + num;
+    }
+}
+
+function inputDecimal(){
+    if(!displayValue.includes('.')){
+        displayValue += '.'
+    }
+}
+
+function clear(){
+    displayValue = '0';
+}
