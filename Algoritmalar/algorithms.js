b237fb459063e548c6c3ccde20387c00c5b25871
Nizami Sevindi <nsevindi87@gmail.com>
2022-11-10 22:00:09 +0100

    Algorithm 1

---
+++ Algoritmalar/algorithms.js
@@ -0,0 +1,3 @@
+import { userValidation } from "./baslangic/1_userValidation.js";
+
+userValidation("First lesson first question")
---
+++ Algoritmalar/baslangic/1_userValidation.js
@@ -0,0 +1,4 @@
+export const userValidation = (str) =>{
+     console.log(str)
+
+}
