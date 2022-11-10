08bac7e1378c6fff55c298541cf8bc11971c3150
Nizami Sevindi <nsevindi87@gmail.com>
2022-11-11 00:03:59 +0100

    Algorithm 6

--- Algoritmalar/baslangic/6_questionMark.js
+++ Algoritmalar/baslangic/6_questionMark.js
@@ -6,18 +6,17 @@ export const questionMark = (str) => {
 
     const newStr = str.replace(/[^0-9?]/g,"")   //0-9 arasindaki rakam ve ? disindakileri degistir.
 
-    const arr = newStr.split("")
+    const arr = newStr.split("")  //yeni array olusturma
 
     let sums = []
     let sum = 0
     let iterator = 0
 
     arr.forEach((item) => {
-        if(item != "?"){
-            sum = parseInt(item) + parseInt(arr[iterator+4])
+        if(item != "?"){       //Soru isaret olmayan karaktere gelince 
+            sum = parseInt(item) + parseInt(arr[iterator+4])  // karakteri ve 4.karakteri topla
             sums.push(sum)
         }
-
         iterator += 1
     })
 
