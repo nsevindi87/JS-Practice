68cccd8ae0cda3c51bbb26d94c4f95bb043d1e9a
Nizami Sevindi <nsevindi87@gmail.com>
2022-11-11 13:11:26 +0100

    Algorithm 10

--- Algoritmalar/baslangic/10_simpleSymbols.js
+++ Algoritmalar/baslangic/10_simpleSymbols.js
@@ -24,5 +24,18 @@ export const simpleSymbols = (str) => {
 }
 
 /* 
+function simpleSymbols(str){
+    let arr = str.toLowerCase().split("");
+    for(var i = 0; i<arr.length; i++){
+        if(arr[i] >= "a" && arr[i] >= "z"){
+            if(i===0 || i===arr.length){
+                return false;
+            }
 
+            if(arr[i-1] !== "+" || arr[i+1] !== "+" ){
+                return false
+            }
+        }
+    }
+}
 */
