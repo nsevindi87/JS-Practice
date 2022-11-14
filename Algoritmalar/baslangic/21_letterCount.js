0fd6da7bf7a743a0e0488944c8865bd292e98592
Nizami Sevindi <nsevindi87@gmail.com>
2022-11-14 21:50:28 +0100

    Algorithm 21

--- Algoritmalar/baslangic/21_letterCount.js
+++ Algoritmalar/baslangic/21_letterCount.js
@@ -7,7 +7,7 @@ export const letterCount = (str) => {
     for (let i = 0; i < arr.length; i++) {
         let obj = {}
 
-        arr[i].split("").forEach(char=>{
+        arr[i].split("").forEach(char => {
             char in obj ? obj[char] += 1 : obj[char] = 1
         });
 
@@ -16,14 +16,27 @@ export const letterCount = (str) => {
 
     let maxFrequencies = []
 
-    kelimeNesneleri.forEach((nesne)=>{
+    kelimeNesneleri.forEach((nesne) => {
         maxFrequencies.push(Math.max(...Object.values(nesne)))
     })
 
     let maxValue = Math.max(...maxFrequencies)
 
-    return maxValue < 2 ? -1 : (arr[maxFrequencies.indexOf(maxValue)]? arr[maxFrequencies.indexOf(maxValue)]: -1)
+    return maxValue < 2 ? -1 : (arr[maxFrequencies.indexOf(maxValue)] ? arr[maxFrequencies.indexOf(maxValue)] : -1)
 
 }
 
 /* Kelime icerisinde birden fazla tekrar eden harfi bul ve sonunda bu harfin icinde oldugu kelimeyi getir. */
+
+/* const newArr = [number, alphabetU, alphabetL, symbols];
+   for (let index = 0; index < 8; index++) {
+    let randomlyNumber = Math.floor(Math.random()*4) ; 
+    for (let j = 0; j < 1; j++) {
+      let randomlyNumber2 = Math.floor(Math.random()*newArr[randomlyNumber].length) ; 
+      if (password.charAt(password.length-1) !== newArr[randomlyNumber][randomlyNumber2]) {
+        password += newArr[randomlyNumber][randomlyNumber2];
+      }else{
+        password += newArr[randomlyNumber][randomlyNumber2+1];
+      }
+    }
+  } */
