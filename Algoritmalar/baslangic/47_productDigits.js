525f0864cc487b5e5aece182132663e3f398452b
Nizami Sevindi <nsevindi87@gmail.com>
2022-11-26 22:38:54 +0100

    algorithym 47

--- Algoritmalar/baslangic/47_productDigits.js
+++ Algoritmalar/baslangic/47_productDigits.js
@@ -1,21 +1,9 @@
-export const productDigits = (pNum) => {
-    let multipliers = []
-    for (let i = 1; i <= pNum; i++) {
-        pNum % i === 0 && multipliers.push(i)
-    }
-
-    let medium = multipliers.length / 2
-
-    if(multipliers[medium-1] * multipliers[medium] === 24){
-        console.log(multipliers[medium-1].toString().length + multipliers[medium].toString().length );
-        console.log(multipliers[medium-1] + multipliers[medium]);
-    }
-    
-    return multipliers
-
+const salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
 }
- 
-/* Parametre sayi carpanlara ayrildi.
-   Ortadaki iki sayi alindi ve kac basamaktan olustugu hesaplanarak yazdirildi.
-    Carpanlardan toplami en düsük olan sayi bulundu.
-*/
+
+let sum = salaries.map(item => item.John + item.Ann + item.Pete)
+
+console.log(sum)
